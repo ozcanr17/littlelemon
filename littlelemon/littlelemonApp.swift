81fc8021d518345b79d391931046e52c262a4d37
ozcanr17 <ridvanozcan7@gmail.com>
2023-11-08 20:32:28 +0300

    Initial Commit

---
+++ littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Rıdvan on 8.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
