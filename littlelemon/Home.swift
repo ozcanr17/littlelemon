1caf6a5ee99e25bea923384ab7ef161bc128f843
ozcanr17 <ridvanozcan7@gmail.com>
2023-11-10 23:06:30 +0300

    Added Home View and Little Lemon assets.

---
+++ littlelemon/Home.swift
@@ -0,0 +1,79 @@
+//
+//  Home.swift
+//  littlelemon
+//
+//  Created by Rıdvan on 10.11.2023.
+//
+
+import SwiftUI
+
+struct JSONMenu: Codable {
+    let menu: [MenuItem]
+}
+
+struct MenuItem: Codable {
+    let name: String
+    let price: Float
+    let description: String
+    let image: String
+}
+
+struct Home: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Spacer()
+                Image("Logo")
+                Spacer()
+                Image("Profile")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40)
+            }
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Little Lemon")
+                        .font(.largeTitle)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    Text("Chicago")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                }
+                Spacer()
+            }
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("We are family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.")
+                        .frame(width: 200)
+                }
+                Image("Hero image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 160, height: 160)
+                    .cornerRadius(20)
+                Spacer()
+            }
+            
+            TabView {
+                MenuView()
+                    .tabItem {
+                        Label("Menu", systemImage: "list.dash")
+                    }
+            }
+            .navigationBarBackButtonHidden(true)
+        }
+        .padding(.horizontal, 10)
+    }
+}
+
+struct MenuView: View {
+    var body: some View {
+        Text("Menu")
+    }
+}
+
+#Preview {
+    Home()
+}
+
--- littlelemon/Onboarding.swift
+++ littlelemon/Onboarding.swift
@@ -18,62 +18,71 @@ struct Onboarding: View {
     @State private var lastName: String = ""
     @State private var email: String = ""
     @State private var showAlert = false
+    @State private var isLoggedIn = false
     
     var body: some View {
-        VStack(alignment: .center) {
-            Text("Welcome to Little Lemon")
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .padding(30)
-            TextField("First Name", text: $firstName)
-                .padding()
-                .frame(width: 300, height: 50)
-                .background(Color.black.opacity(0.05))
-                .cornerRadius(10)
-            TextField("Last Name", text: $lastName)
-                .padding()
-                .frame(width: 300, height: 50)
-                .background(Color.black.opacity(0.05))
-                .cornerRadius(10)
-            TextField("Email", text: $email)
-                .keyboardType(.emailAddress)
-                .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
-                .disableAutocorrection(true)
-                .padding()
-                .frame(width: 300, height: 50)
-                .background(Color.black.opacity(0.05))
-                .cornerRadius(10)
-            Button("Register") {
-                if !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty {
+        NavigationView {
+            VStack(alignment: .center) {
+                NavigationLink(destination: Home(), isActive: $isLoggedIn) {
+                    EmptyView()
+                }
+                Text("Welcome to Little Lemon")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .padding(30)
+                TextField("First Name", text: $firstName)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(10)
+                TextField("Last Name", text: $lastName)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(10)
+                TextField("Email", text: $email)
+                    .keyboardType(.emailAddress)
+                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                    .disableAutocorrection(true)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(10)
+                Button("Register") {
+                    if !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty {
+                        isLoggedIn = true
                         UserDefaults.standard.set(firstName, forKey: uniqueFirstName)
                         UserDefaults.standard.set(lastName, forKey: uniqueLastName)
                         UserDefaults.standard.set(email, forKey: uniqueEmail)
                     } else {
                         if email.isEmpty || !isValidEmail(email) {
-                        showAlert = true
+                            showAlert = true
+                        }
                     }
                 }
+                .padding()
+                .foregroundColor(.white)
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 40)
+                .background(Color.black)
+                .cornerRadius(10)
+                .alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("Invaild Email"),
+                        message: Text("Please provide a valid e-mail address.")
+                    )
+                }
             }
-            .padding()
-            .foregroundColor(.white)
-            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 40)
-            .background(Color.black)
-            .cornerRadius(10)
-            .alert(isPresented: $showAlert) {
-                Alert(
-                    title: Text("Invaild Email"),
-                    message: Text("Please provide a valid e-mail address.")
-                )
-            }
+            .padding(.horizontal)
+            .background(ignoresSafeAreaEdges: .all)
         }
-        .padding(.horizontal)
-        .background(ignoresSafeAreaEdges: .all)
     }
+    
     func isValidEmail(_ email: String) -> Bool {
-      let emailValidationRegex = "^[\\p{L}0-9!#$%&'*+\\/=?^_`{|}~-][\\p{L}0-9.!#$%&'*+\\/=?^_`{|}~-]{0,63}@[\\p{L}0-9-]+(?:\\.[\\p{L}0-9-]{2,7})*$"
-      let emailValidationPredicate = NSPredicate(format: "SELF MATCHES %@", emailValidationRegex)
-      return emailValidationPredicate.evaluate(with: email)
-    }}
+        let emailValidationRegex = "^[\\p{L}0-9!#$%&'*+\\/=?^_`{|}~-][\\p{L}0-9.!#$%&'*+\\/=?^_`{|}~-]{0,63}@[\\p{L}0-9-]+(?:\\.[\\p{L}0-9-]{2,7})*$"
+        let emailValidationPredicate = NSPredicate(format: "SELF MATCHES %@", emailValidationRegex)
+        return emailValidationPredicate.evaluate(with: email)
+    }
+}
 
 #Preview {
     Onboarding()
